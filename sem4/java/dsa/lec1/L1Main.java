f93b746069326579899e13f2243f4926fd079fce
Gautam Yadav <gautamyadav08112005@gmail.com>
2024-01-09 09:34:59 +0530

    lec 1 done

---
+++ sem4/java/dsa/lec1/L1Main.java
@@ -0,0 +1,19 @@
+public class L1Main {
+    public static void main(String[] args) {
+        // Printable p = () -> {
+        // System.out.println("Lembda Method called from Printable Interface.");
+        // return "Hello Lembda";
+        // };
+
+        // System.out.println(p.m1());
+
+        // Generics<String, Integer> x = s -> s.length();
+
+        // System.out.println("Length = " + x.m1("Gautam"));
+
+        // Predicate<Integer> z = c -> c % 2 == 0;
+
+        // z.test(100);
+
+    }
+}
