1f21eef5eb8d9d213b16867e28e8e5e99eba5cdc
Gautam Yadav <gautamyadav08112005@gmail.com>
2024-02-26 11:32:17 +0530

    sorting done

--- sem4/dsa/lec16_notations/pack/DoublyLinkedList.java
+++ sem4/dsa/lec16_notations/pack/DoublyLinkedList.java
@@ -9,6 +9,26 @@ public class DoublyLinkedList {
         return head == null;
     }
 
+    public void sortOnCPI() {
+        if (isEmpty() || head.next == null) {
+            return;
+        }
+
+        Node f = head;
+        while (f != null) {
+            Node current = head;
+            while (current.next != null) {
+                if (current.data.cpi < current.next.data.cpi) {
+                    Student temp = current.data;
+                    current.data = current.next.data;
+                    current.next.data = temp;
+                }
+                current = current.next;
+            }
+            f = f.next;
+        }
+    }
+
     public void addFirst(Student data) {
         Node np = new Node(data);
 
