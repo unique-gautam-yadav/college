b01e07b776d1a885212d95bc7dd4a9db628e1f7b
Gautam Yadav <gautamyadav08112005@gmail.com>
2024-05-01 20:41:28 +0530

    ft.  and  done

--- sem4/dsa/lec19_graph/AdjacencyList.java
+++ sem4/dsa/lec19_graph/AdjacencyList.java
@@ -1,4 +1,9 @@
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Queue;
+import java.util.Stack;
 
 public class AdjacencyList {
     ArrayList<Integer>[] graph;
@@ -13,6 +18,79 @@ public class AdjacencyList {
             graph[i] = new ArrayList<Integer>();
         }
 
+    }
+
+    public boolean hasLoop(int u) {
+        Map<Integer, Boolean> visited = new HashMap<Integer, Boolean>(graph.length);
+
+        Stack<Integer> q = new Stack<Integer>();
+        q.push(u);
+        while (!q.isEmpty()) {
+            Integer v = q.pop();
+            if (visited.get(v) == null || !visited.get(v)) {
+                visited.put(v, true);
+                for (Integer i : graph[v]) {
+                    boolean notVisited = visited.get(i) == null || !visited.get(i);
+
+                    if (notVisited && q.contains(i)) {
+                        return true;
+                    }
+                    if (notVisited) {
+                        q.push(i);
+                    } else {
+                        if (q.contains(i)) {
+                            return true;
+                        }
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+    public void bfs(int u) {
+        // boolean[] isVisited = new boolean[graph.length];
+        Map<Integer, Boolean> visited = new HashMap<Integer, Boolean>(graph.length);
+        Queue<Integer> q = new LinkedList<Integer>();
+        q.offer(u);
+        while (!q.isEmpty()) {
+            Integer v = q.poll();
+            if (visited.get(v) == null || !visited.get(v)) {
+                // if (!isVisited[v]) {
+                // isVisited[v] = true;
+                visited.put(v, true);
+                System.out.print(v + "  ");
+                for (Integer i : graph[v]) {
+                    if (visited.get(i) == null || !visited.get(i))
+                        // if (!isVisited[i])
+                        q.offer(i);
+                }
+            }
+        }
+        System.out.println();
+    }
+
+    public void dfs(int u) {
+        // boolean[] isVisited = new boolean[graph.length];
+        Map<Integer, Boolean> visited = new HashMap<Integer, Boolean>(graph.length);
+
+        Stack<Integer> q = new Stack<Integer>();
+        q.push(u);
+        while (!q.isEmpty()) {
+            Integer v = q.pop();
+            if (visited.get(v) == null || !visited.get(v)) {
+                // if (!isVisited[v]) {
+                // isVisited[v] = true;
+                visited.put(v, true);
+                System.out.print(v + "  ");
+                for (Integer i : graph[v]) {
+                    // if (!isVisited[i])
+                    if (visited.get(i) == null || !visited.get(i))
+                        q.push(i);
+                }
+            }
+        }
+        System.out.println();
     }
 
     /**
--- sem4/dsa/lec19_graph/Main.java
+++ sem4/dsa/lec19_graph/Main.java
@@ -1,33 +1,47 @@
 public class Main {
-    // public static void main(String[] args) {
-    //     AdjacencyList graph = new AdjacencyList(5);
-    //     graph.addEdge(0, 1);
-    //     graph.addEdge(0, 2);
-
-    //     graph.addEdge(1, 2);
-    //     graph.addEdge(1, 3);
-
-    //     graph.addEdge(2, 4);
-        
-    //     graph.addEdge(4, 3);
-
-    //     graph.print();
-    // }
-
     public static void main(String[] args) {
-        EdgeList graph = new EdgeList(5);
+        AdjacencyList graph = new AdjacencyList(4);
+
         graph.addEdge(0, 1);
-        graph.addEdge(0, 2);
-
         graph.addEdge(1, 2);
-        graph.addEdge(1, 3);
+        graph.addEdge(2, 3);
+        // graph.addEdge(3, 0);
 
-        graph.addEdge(2, 4);
-        
-        graph.addEdge(4, 3);
+        System.out.println(graph.hasLoop(0));
 
-        graph.print();
+        // AdjacencyList graph = new AdjacencyList(5);
+        // graph.addEdge(0, 1);
+        // graph.addEdge(0, 2);
+
+        // graph.addEdge(1, 2);
+        // graph.addEdge(1, 3);
+
+        // graph.addEdge(2, 4);
+
+        // graph.addEdge(4, 3);
+
+        // System.out.println("BFS");
+        // graph.bfs(1);
+        // System.out.println("DFS");
+        // graph.dfs(1);
+        // System.out.println(graph.hasLoop(0));
     }
+
+    // public static void main(String[] args) {
+    // EdgeList graph = new EdgeList(5);
+    // graph.addEdge(0, 1);
+    // graph.addEdge(0, 2);
+
+    // graph.addEdge(1, 2);
+    // graph.addEdge(1, 3);
+
+    // graph.addEdge(2, 4);
+
+    // graph.addEdge(4, 3);
+
+    // graph.print();
+
+    // }
 }
 
 /**
